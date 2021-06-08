d9cf0413e7e5a50e8c978afd0bc173661dc9dd2a
7204-OMER <Eyupbhr24@gmail.com>
2021-06-08 14:08:38 +0300

    add axios func

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import "./App.css";
 import Form from "./components/form/Form";
 import Header from "./components/header/Header";
 import JobCard from "./components/jobcard/JobCard";
+import axios from "axios";
 
 function App() {
+  const newQuery = () => {
+    axios({
+      method: "get",
+      url: `/position.json?description=${"engineer"}&location=${"usa"}`,
+    }).then((res) => console.log(res));
+  };
+
   const dummy = {
     company_logo:
       "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftr.linkedin.com%2Fcompany%2Fclarusway%3Ftrk%3Dpublic_profile_topcard-current-company&psig=AOvVaw2-YlTPNy_8bd5p6SxpUL3r&ust=1623234800485000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCH4NXqh_ECFQAAAAAdAAAAABAD",
